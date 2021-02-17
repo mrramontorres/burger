1da505986512f9f8605351e948387b057d25da6e
turtles <me@ramon.nyc>
2021-02-16 23:06:21 -0800

    set up model js file to match exercise class sans delete and updated for new orm methods

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,39 @@
 // Import MySQL connection.
 const connection = require('./connection.js');
 
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = (num) => {
+    const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push('?');
+    }
+  
+    return arr.toString();
+  };
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  const objToSql = (ob) => {
+    const arr = [];
+  
+    // Loop through the keys and push the key/value as a string int arr
+    for (const key in ob) {
+      let value = ob[key];
+      // Check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // If string with spaces, add quotations (Grilled Tofu Burger => 'Grilled Tofu Burger')
+        if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+          value = `'${value}'`;
+        }
+        // e.g. {name: 'Grilled Tofu Burger'} => ["name='Grilled Tofu Burger'"]
+        // e.g. {devoured: true} => ["devoured=true"]
+        arr.push(`${key}=${value}`);
+      }
+    }
+  
+    // Translate array of strings to a single comma-separated string
+    return arr.toString();
+  };
 
 // Object for all our SQL statement functions.
 const orm = {
