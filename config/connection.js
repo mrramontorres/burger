bc75bf052b845017471bcfc9c4232093fe96d438
turtles <me@ramon.nyc>
2021-02-17 21:17:27 -0800

    added main and index handlebar exercise source code,

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: Process.env.DOCSERV,
+  password: process.env.DOCSERV,
   database: 'burgers_db',
 });
 
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,40 +7,40 @@ const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
-    burger.selectAll((data) => {
-      const hbsObject = {
-        burger: data,
-      };
-      console.log(hbsObject);
-      res.render('index', hbsObject);
-    });
+  burger.selectAll((data) => {
+    const hbsObject = {
+      burger: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
 });
 
 router.post('/api/burgers', (req, res) => {
-    burger.insertOne(['name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
-        // Send back the ID of the new burger added
-        res.json({ id: result.insertId });
-    });
+  burger.insertOne(['name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
+    // Send back the ID of the new burger added
+    res.json({ id: result.insertId });
+  });
 });
 
 router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
-  
-    console.log('condition', condition);
-  
-    burger.updateOne(
-      {
-        devoured: req.body.devoured,
-      },
-      condition,
-      (result) => {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
       }
-    );
+      res.status(200).end();
+    }
+  );
 });
 
 // Export routes for server.js to use.
